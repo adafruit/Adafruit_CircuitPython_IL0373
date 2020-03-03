9a60314368d1df04d78c21a1343962377b6a9012
dherrada <dylan.herrada@gmail.com>
2020-03-03 15:51:28 -0500

    Ran black, made pylint 2.x compatible

--- examples/il0373_flexible_2.9_monochrome.py
+++ examples/il0373_flexible_2.9_monochrome.py
@@ -13,18 +13,20 @@ import adafruit_il0373
 displayio.release_displays()
 
 # This pinout works on a Feather M4 and may need to be altered for other boards.
-spi = board.SPI() # Uses SCK and MOSI
+spi = board.SPI()  # Uses SCK and MOSI
 epd_cs = board.D9
 epd_dc = board.D10
 epd_reset = board.D5
 epd_busy = board.D6
 
-display_bus = displayio.FourWire(spi, command=epd_dc, chip_select=epd_cs, reset=epd_reset,
-                                 baudrate=1000000)
+display_bus = displayio.FourWire(
+    spi, command=epd_dc, chip_select=epd_cs, reset=epd_reset, baudrate=1000000
+)
 time.sleep(1)
 
-display = adafruit_il0373.IL0373(display_bus, width=296, height=128, rotation=90, busy_pin=epd_busy,
-                                 swap_rams=True)
+display = adafruit_il0373.IL0373(
+    display_bus, width=296, height=128, rotation=90, busy_pin=epd_busy, swap_rams=True
+)
 
 g = displayio.Group()
 
